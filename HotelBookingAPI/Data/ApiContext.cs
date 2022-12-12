f453c9f55517fb9907254c082e3ed99aa913d7ee
Kenny Tran <kennyytran95@gmail.com>
2022-12-12 01:26:06 -0800

    Add project files.

---
+++ HotelBookingAPI/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+                :base(options)
+        { 
+        
+        }
+    }
+}
